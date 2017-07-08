640436fce106fd0bc16ea1241a849d074cc1710e
Rizwan Aslam(Am) <rizwan@codebrisk.com>
2017-07-09 02:17:57 +0500

    portfolio PNG removed

--- resources/views/frontend/partials/models.blade.php
+++ resources/views/frontend/partials/models.blade.php
@@ -15,7 +15,7 @@
                         <div class="modal-body">
                             <h2>Say It With A Sock</h2>
                             <p class="item-intro text-muted">Great and fun socks.</p>
-                            <img class="img-responsive img-centered" src="img/portfolio/sayitsock2.jpg" alt="">
+                            <img class="img-responsive img-centered" src="img/portfolio/sayitsock-model.JPG" alt="">
                             <p><a href="https://portal.sayitwithasock.com/">SayItWithAsock</a> This application is built in laravel and this application tightly integrated with cratejoy subscriptions systems, and we created a system that allocate dynamic allocate product to shipments of the orders. and created feedback system. we integrated couple of third party libraries with this application like shipstation, elasticsearch, klaviyo, grooveHq etc.</p>
                             <p>You can preview the site here <a href="https://portal.sayitwithasock.com/">here</a>.</p>
                             <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
@@ -43,7 +43,7 @@
                         <div class="modal-body">
                             <h2>Job Seeker</h2>
                             <p class="item-intro text-muted">find any job here.</p>
-                            <img class="img-responsive img-centered" src="img/portfolio/jobseeker-model.png" alt="">
+                            <img class="img-responsive img-centered" src="img/portfolio/jobseeker-model.PNG" alt="">
                             <p><a href="https://job-cy.com/en">Job Seeker</a> This application is built in laravel and this application tightly integrated with excel import/export.</p>
                             <p>You can preview the site here <a href="https://job-cy.com/en">here</a>.</p>
                             <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
@@ -72,7 +72,7 @@
                             <!-- Project Details Go Here -->
                             <h2>Sage Property</h2>
                             <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                            <img class="img-responsive img-centered" src="img/portfolio/sage-model.png" alt="">
+                            <img class="img-responsive img-centered" src="img/portfolio/sage-model.PNG" alt="">
                             <p>Understanding your information improves our search for the right property just for you. We offer the first-rate options relevant to your preference and affordability.</p>
                             <p>Searching for your dream home can be time consuming but at Sage, we do the hunt, you do the pick. see  <a href="http://sage-property.seersol.com/">sageProperty.com</a>.</p>
                             <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
